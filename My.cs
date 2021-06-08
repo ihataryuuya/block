62933899ccce22d9a42e87790cca220a0c128bc2
バーサーカーソ\vantan <rahu_tarou@outlook.p>
2021-06-08 15:44:16 +0900

    ブロック崩し

---
+++ My.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class My : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Sphere;
+    [SerializeField]
+    private GameObject SphereIti;
+    static public bool ballExistFlag = false;
+    float accel = 1000.0f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballExistFlag = false;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.right * Input.GetAxis("Horizontal") * accel,
+            ForceMode.Impulse);
+        if (Input.GetKeyDown("space"))
+        {
+            if (!ballExistFlag)
+            {
+                Instantiate(Sphere,
+                    SphereIti.transform.position,
+                    SphereIti.transform.rotation);
+                ballExistFlag = true;
+            }
+        }
+    }
+}
